8197d2fef74c894921741e210a9670255105021e
manhnguyen <manhcan03@gmail.com>
2025-04-29 22:28:27 +0700

    update version

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, createContext } from "react";
 import PointComponent from "./components/PointComponent";
+
+export const MyContext = createContext();
 
 function App() {
   const [pendingPoint, setPendingPoint] = useState("");
@@ -8,20 +10,26 @@ function App() {
   const [currentClickIndex, setCurrentClickIndex] = useState(1);
   const [gameStatus, setGameStatus] = useState("");
   const [clickedPoints, setClickedPoints] = useState([]);
-  const [titleButton, setTitleButton] = useState('Play');
+  const [titleButton, setTitleButton] = useState("Play");
+  const [isGameOver, setisGameOver] = useState(false);
   const timerRef = useRef(null);
+  const timerIdRef = useRef(null);
+  const autoPlayRef = useRef(null);
+  const [isAutoPlay, setIsAutoPlay] = useState(false);
 
   const handlePointChange = (e) => {
     setPendingPoint(e.target.value);
   };
 
   const handleRestart = () => {
-    setTitleButton('Restart');
+    setIsAutoPlay(false);
+    setisGameOver(false);
+    setTitleButton("Restart");
     const parsed = parseInt(pendingPoint);
     if (!isNaN(parsed)) {
       const positions = Array.from({ length: parsed }, (_, index) => ({
-        top: Math.random() * 92,
-        left: Math.random() * 92,
+        top: Math.random() * 90,
+        left: Math.random() * 90,
         value: index + 1,
         clicked: false,
       }));
@@ -45,22 +53,25 @@ function App() {
   };
 
   const handlePointClick = (value) => {
-    if (gameStatus) return; 
+    setisGameOver(false);
+    if (gameStatus) return;
 
     if (value === currentClickIndex) {
       setClickedPoints((prev) => [...prev, value]);
-      
-      setTimeout(() => {
+
+      timerIdRef.current = setTimeout(() => {
         setPointsPosition((prevPositions) =>
           prevPositions.map((pos) =>
             pos.value === value ? { ...pos, clicked: true } : pos
           )
         );
-      }, 1000); 
+      }, 3000);
 
       if (value === pointsPosition.length) {
-        setGameStatus("win");
-        clearInterval(timerRef.current);
+        setTimeout(() => {
+          setGameStatus("win");
+          clearInterval(timerRef.current);
+        }, 600);
       } else {
         setCurrentClickIndex((prev) => prev + 1);
       }
@@ -70,6 +81,65 @@ function App() {
     }
   };
 
+  const handleAutoPlay = () => {
+    if (autoPlayRef.current) {
+      setIsAutoPlay(false);
+    }
+
+    if (isAutoPlay) {
+      // Tắt AutoPlay nếu đang bật
+      setIsAutoPlay(false);
+      if (autoPlayRef.current) {
+        clearInterval(autoPlayRef.current);
+      }
+      console.log("currenidex: ", currentClickIndex);
+    } else {
+      // Bật AutoPlay
+      if (currentClickIndex > 1) {
+        setIsAutoPlay(true);
+      }
+      if (pointsPosition.length > 0) {
+        setIsAutoPlay(true);
+      } else {
+        handleRestart();
+        setIsAutoPlay(true);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (!isAutoPlay) {
+      clearInterval(autoPlayRef.current);
+    }
+    if (isAutoPlay && pointsPosition.length > 0) {
+      if (autoPlayRef.current) {
+        clearInterval(autoPlayRef.current);
+      }
+
+      autoPlayRef.current = setInterval(() => {
+        if (gameStatus === "win" || gameStatus === "gameover") {
+          clearInterval(autoPlayRef.current);
+          setCurrentClickIndex(1);
+          setIsAutoPlay(false);
+          return;
+        }
+        if (currentClickIndex <= pointsPosition.length) {
+          handlePointClick(currentClickIndex);
+        }
+      }, 1200);
+      if (gameStatus === "win") {
+        clearInterval(autoPlayRef.current);
+        setCurrentClickIndex(1);
+        setIsAutoPlay(false);
+      }
+      if (gameStatus === "gameover") {
+        clearInterval(autoPlayRef.current);
+        setCurrentClickIndex(1);
+        setIsAutoPlay(false);
+      }
+    }
+  }, [isAutoPlay, pointsPosition, currentClickIndex, gameStatus]);
+
   useEffect(() => {
     return () => {
       if (timerRef.current) {
@@ -77,64 +147,88 @@ function App() {
       }
     };
   }, []);
+  useEffect(() => {
+    if (gameStatus === "gameover") {
+      clearTimeout(timerIdRef.current);
+      clearInterval(timerRef.current);
+      setisGameOver(true);
+    }
+  }, [gameStatus]);
+  //
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
-      <div className="mx-auto p-4 border border-gray-300 rounded-lg shadow-lg bg-white">
-        <h1 className={`text-xl font-bold mb-2 ${gameStatus === 'win' ? "text-green-600": ''} ${gameStatus === 'gameover' ? "text-red-500" : ''}`}>
-          {gameStatus === "" && "LET'S PLAY"}
-          {gameStatus === "win" && "ALL CLEARED"}
-          {gameStatus === "gameover" && "GAME OVER"}
-        </h1>
-        <div className="flex flex-row items-center gap-6">
-          <div className="flex flex-col">
-            <label className="mr-2">Points:</label>
-            <p>Time: </p>
-          </div>
-          <div className="flex flex-col">
-            <input
-              type="text"
-              value={pendingPoint}
-              onChange={handlePointChange}
-              placeholder="0"
-              className="pl-1 border border-gray-300 rounded-md w-32"
-            />
-            <p>{time.toFixed(1)}s</p>
-          </div>
-        </div>
-        <button
-          className="bg-gray-100 hover:bg-gray-300 text-gray-800 font-bold px-5 rounded cursor-pointer border"
-          onClick={handleRestart}
-        >
-          {titleButton}
-        </button>
-
-        <div
-          className="border border-gray-800 mt-4 relative p-1 md:w-[400px] sm:w-[400px] xl:w-[400px] h-[400px] w-[300px]"
-          // style={{ width: "400px", height: "400px" }}
-        >
-          {pointsPosition.map((pos, index) => (
-            <div
-              key={index}
-              className="absolute transition-all duration-1000 ease-in-out"
-              style={{
-                top: `${pos.top}%`,
-                left: `${pos.left}%`,
-                zIndex: pointsPosition.length - index,
-                opacity: pos.clicked ? 0 : 1,
-                transform: pos.clicked ? 'scale(0.8)' : 'scale(1)',
-              }}
-            >
-              <PointComponent
-                point={pos.value}
-                handlePointClick={() => handlePointClick(pos.value)}
-                isCurrent={clickedPoints.includes(pos.value)}
-              />
+    <MyContext.Provider value={{ isGameOver, setisGameOver }}>
+      <div className="min-h-screen flex flex-col items-center justify-center">
+        <div className="mx-auto p-4 border border-gray-300 rounded-lg shadow-lg bg-white">
+          <h1
+            className={`text-xl font-bold mb-2 ${
+              gameStatus === "win" ? "text-green-600" : ""
+            } ${gameStatus === "gameover" ? "text-red-500" : ""}`}
+          >
+            {gameStatus === "" && "LET'S PLAY"}
+            {gameStatus === "win" && "ALL CLEARED"}
+            {gameStatus === "gameover" && "GAME OVER"}
+          </h1>
+          <div className="flex flex-row items-center gap-6">
+            <div className="flex flex-col">
+              <label className="mr-2">Points:</label>
+              <p>Time: </p>
             </div>
-          ))}
+            <div className="flex flex-col">
+              <input
+                type="text"
+                value={pendingPoint}
+                onChange={handlePointChange}
+                placeholder="0"
+                className="pl-1 border border-gray-300 rounded-md w-32"
+              />
+              <p>{time.toFixed(1)}s</p>
+            </div>
+          </div>
+          <div className="flex flex-row gap-3">
+            <button
+              className="bg-gray-100 hover:bg-gray-300 text-gray-800 font-bold px-5 rounded cursor-pointer border"
+              onClick={handleRestart}
+            >
+              {titleButton}
+            </button>
+            {titleButton === "Restart" && (
+              <button
+                className="bg-gray-100 hover:bg-gray-300 text-gray-800 font-bold px-5 rounded cursor-pointer border"
+                onClick={handleAutoPlay}
+              >
+                {isAutoPlay ? "Auto Play On " : "Auto Play Off"}
+              </button>
+            )}
+          </div>
+          <div
+            className="border border-gray-800 mt-4 relative p-1 md:w-[400px] sm:w-[400px] xl:w-[400px] h-[400px] w-[300px]"
+            // style={{ width: "400px", height: "400px" }}
+          >
+            {pointsPosition.map((pos, index) => (
+              <div
+                key={index}
+                className="absolute "
+                style={{
+                  top: `${pos.top}%`,
+                  left: `${pos.left}%`,
+                  zIndex: pointsPosition.length - index,
+                  opacity: pos.clicked ? 0 : 1,
+                  transform: pos.clicked ? "scale(0.8)" : "scale(1)",
+                }}
+              >
+                <PointComponent
+                  point={pos.value}
+                  handlePointClick={() => handlePointClick(pos.value)}
+                  isCurrent={clickedPoints.includes(pos.value)}
+                  isAutoPlay={isAutoPlay}
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </MyContext.Provider>
   );
 }
 
--- src/components/PointComponent.jsx
+++ src/components/PointComponent.jsx
@@ -1,21 +1,99 @@
-const PointComponent = ({ point, handlePointClick, isCurrent }) => {
+import { useContext, useEffect, useRef, useState } from "react";
+import { MyContext } from "../App";
+
+const PointComponent = ({ point, handlePointClick, isCurrent, isAutoPlay }) => {
+  const [timeDown, setTimeDown] = useState(0.0);
+  const [opacity, setOpacity] = useState(1.0);
+  const intervalRef = useRef(null);
+  const timeoutRef = useRef(null);
+  const { isGameOver } = useContext(MyContext);
+  const handleClick = () => {
+    setTimeDown(0.0);
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    }
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+      timeoutRef.current = null;
+    }
+
+    intervalRef.current = setInterval(() => {
+      setTimeDown((prev) => parseFloat((prev + 0.1).toFixed(1)));
+    }, 100);
+
+    // if (!isAutoPlay) {
+    handlePointClick();
+    // }
+
+    timeoutRef.current = setTimeout(() => {
+      clearInterval(intervalRef.current);
+      setTimeDown(0.0);
+    }, 3000);
+  };
+  useEffect(() => {
+    if (isGameOver) {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+      }
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+        timeoutRef.current = null;
+      }
+    } else {
+      setTimeDown(0.0);
+    }
+  }, [isGameOver]);
+
+  useEffect(() => {
+    if (isAutoPlay && isCurrent) {
+      setTimeDown(0.0);
+
+      setOpacity(1.0);
+
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+
+      intervalRef.current = setInterval(() => {
+        setTimeDown((prev) => parseFloat((prev + 0.1).toFixed(1)));
+      }, 100);
+
+      timeoutRef.current = setTimeout(() => {
+        clearInterval(intervalRef.current);
+        setTimeDown(0.0);
+      }, 3000);
+    }
+  }, [isAutoPlay, isCurrent]);
+  //
+  useEffect(() => {
+    setOpacity(3 - timeDown);
+  }, [timeDown]);
   return (
     <div
       style={{
         borderRadius: "50%",
-        width: "30px",
-        height: "30px",
+        width: "40px",
+        height: "40px",
         display: "flex",
+        flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
         border: "1px solid black",
-        backgroundColor: isCurrent ? "red" : "white",
+        backgroundColor: isCurrent ? "orange" : "white",
         cursor: "pointer",
         transition: "background-color 0.3s ease",
+        fontSize: "12px",
+        opacity: opacity,
       }}
-      onClick={handlePointClick}
+      onClick={handleClick}
     >
       {point}
+      {timeDown > 0 && <p className="text-white">{timeDown}</p>}
     </div>
   );
 };
